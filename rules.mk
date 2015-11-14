4ccbd186d89beb774c78512c79db5e6601b99e72
Qianqian Xing <alps_xing@hotmail.com>
2015-11-13 18:14:00 -0800

    Add build for bbb.

--- rules.mk
+++ rules.mk
@@ -8,9 +8,7 @@ BOOST_DIR := $(EXTERNAL_LIB_DIR)/boost
 OPENSSL_DIR := $(EXTERNAL_LIB_DIR)/openssl
 SQLITE_DIR := $(EXTERNAL_LIB_DIR)/sqlite
 CPP_NETLIB_DIR := $(EXTERNAL_LIB_DIR)/cpp-netlib
-CURRENT_DIR = $(shell pwd)
-
-
+TOOLS_DIR = $(BASE_DIR)/tools
 
 ifeq ($(ARCH), x86_64)
 CC=gcc
@@ -18,7 +16,11 @@ else
 ifeq ($(ARCH), arm)
 CC=arm-linux-gnueabi-gcc
 else
+ifeq ($(ARCH), bbb)
+CC=$(TOOLS_DIR)/gcc-linaro-arm-linux-gnueabihf-4.8-2013.10_linux/bin/arm-linux-gnueabihf-gcc
+else
 $(error $(ARCH) is not supported yet.)
+endif
 endif
 endif
 
