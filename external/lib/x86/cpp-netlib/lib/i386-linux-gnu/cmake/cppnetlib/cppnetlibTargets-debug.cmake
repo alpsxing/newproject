3a2ee80fbee00a2e53975308388c5d51cdab7dea
Qianqian Xing <alps_xing@hotmail.com>
2016-01-13 19:06:51 -0800

    Add support for build on x86

---
+++ external/lib/x86/cpp-netlib/lib/i386-linux-gnu/cmake/cppnetlib/cppnetlibTargets-debug.cmake
@@ -0,0 +1,40 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "cppnetlib-uri" for configuration "Debug"
+set_property(TARGET cppnetlib-uri APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(cppnetlib-uri PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "/home/xingqq/newproject/external/lib/x86/cpp-netlib/lib/i386-linux-gnu/libcppnetlib-uri.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS cppnetlib-uri )
+list(APPEND _IMPORT_CHECK_FILES_FOR_cppnetlib-uri "/home/xingqq/newproject/external/lib/x86/cpp-netlib/lib/i386-linux-gnu/libcppnetlib-uri.a" )
+
+# Import target "cppnetlib-server-parsers" for configuration "Debug"
+set_property(TARGET cppnetlib-server-parsers APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(cppnetlib-server-parsers PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "/home/xingqq/newproject/external/lib/x86/cpp-netlib/lib/i386-linux-gnu/libcppnetlib-server-parsers.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS cppnetlib-server-parsers )
+list(APPEND _IMPORT_CHECK_FILES_FOR_cppnetlib-server-parsers "/home/xingqq/newproject/external/lib/x86/cpp-netlib/lib/i386-linux-gnu/libcppnetlib-server-parsers.a" )
+
+# Import target "cppnetlib-client-connections" for configuration "Debug"
+set_property(TARGET cppnetlib-client-connections APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(cppnetlib-client-connections PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LINK_INTERFACE_LIBRARIES_DEBUG "/home/xingqq/newproject/external/lib/x86/openssl/lib/libssl.so;/home/xingqq/newproject/external/lib/x86/openssl/lib/libcrypto.so;/home/xingqq/newproject/external/lib/x86/boost/lib/libboost_unit_test_framework.a;/home/xingqq/newproject/external/lib/x86/boost/lib/libboost_system.a;/home/xingqq/newproject/external/lib/x86/boost/lib/libboost_regex.a;/home/xingqq/newproject/external/lib/x86/boost/lib/libboost_date_time.a;/home/xingqq/newproject/external/lib/x86/boost/lib/libboost_thread.a;/home/xingqq/newproject/external/lib/x86/boost/lib/libboost_filesystem.a;/home/xingqq/newproject/external/lib/x86/boost/lib/libboost_program_options.a;/home/xingqq/newproject/external/lib/x86/boost/lib/libboost_chrono.a;/home/xingqq/newproject/external/lib/x86/boost/lib/libboost_atomic.a;/usr/lib/i386-linux-gnu/libpthread.so"
+  IMPORTED_LOCATION_DEBUG "/home/xingqq/newproject/external/lib/x86/cpp-netlib/lib/i386-linux-gnu/libcppnetlib-client-connections.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS cppnetlib-client-connections )
+list(APPEND _IMPORT_CHECK_FILES_FOR_cppnetlib-client-connections "/home/xingqq/newproject/external/lib/x86/cpp-netlib/lib/i386-linux-gnu/libcppnetlib-client-connections.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
--- rules.mk
+++ rules.mk
@@ -1,8 +1,15 @@
+ifneq (,$(findstring x86_64, $(shell uname -a)))
 ARCH ?= x86_64
+HOSTARCH = x86_64
+else
+ARCH ?= x86
+HOSTARCH = x86
+endif
 BASE_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 MAKEFILE_DIR := $(dir $(abspath $(firstword $(MAKEFILE_LIST))))
 EXTERNAL_DIR := $(BASE_DIR)external
 EXTERNAL_LIB_DIR := $(EXTERNAL_DIR)/lib/$(ARCH)
+
 BOOST_DIR := $(EXTERNAL_LIB_DIR)/boost
 OPENSSL_DIR := $(EXTERNAL_LIB_DIR)/openssl
 SQLITE_DIR := $(EXTERNAL_LIB_DIR)/sqlite
@@ -12,15 +19,25 @@ INSTALL_DIR = $(BASE_DIR)install
 ARICRACK_DIR = $(EXTERNAL_LIB_DIR)/aircrack-ng
 LIBNL_DIR = $(EXTERNAL_LIB_DIR)/libnl
 
+CFLAGS=
+LDFLAGS=
 ifeq ($(ARCH), x86_64)
 CC=gcc
 CXX=g++
 NETLIBDIR=x86_64-linux-gnu
+ifeq ($(HOSTARCH), x86)
+$(error Can not build x86_64 on x86 host))
+endif
 else
-ifeq ($(ARCH), arm)
-CC=arm-linux-gnueabi-gcc
-CXX=arm-linux-gnueabi-g++
-NETLIBDIR=
+ifeq ($(ARCH), x86)
+$(info Building x86 on x86_64 host)
+CC=gcc
+CXX=g++
+ifeq ($(HOSTARCH), x86_64)
+CFLAGS += -m32
+LDFLAGS += -m32
+endif
+NETLIBDIR=i386-linux-gnu
 else
 ifeq ($(ARCH), bbb)
 CC=$(TOOLS_DIR)/gcc-linaro-arm-linux-gnueabihf-4.8-2013.10_linux/bin/arm-linux-gnueabihf-gcc
@@ -33,12 +50,12 @@ endif
 endif
 
 SOURCE_DIR = $(BASE_DIR)src
-CFLAGS = -I$(BOOST_DIR)/include -I/$(OPENSSL_DIR)/include -I/$(ARICRACK_DIR)/include
+CFLAGS += -I$(BOOST_DIR)/include -I/$(OPENSSL_DIR)/include -I/$(ARICRACK_DIR)/include
 CFLAGS += -I$(SQLITE_DIR)/include -I$(CPP_NETLIB_DIR)/include
 CFLAGS += -I$(LIBNL_DIR)/include
 CFLAGS += -fpermissive
 
-LDFLAGS = -L$(CPP_NETLIB_DIR)/lib/$(NETLIBDIR)
+LDFLAGS += -L$(CPP_NETLIB_DIR)/lib/$(NETLIBDIR)
 LDFLAGS += -L$(BOOST_DIR)/lib
 LDFLAGS += -L$(OPENSSL_DIR)/lib -L$(SQLITE_DIR)/lib -L$(ARICRACK_DIR)/lib
 LDFLAGS += -L$(LIBNL_DIR)/lib
